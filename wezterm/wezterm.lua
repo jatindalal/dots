c19f4aa58c2ec81bafa22e972bff6dba9369a794
jdj <jd@jd.jd>
2024-09-25 20:29:29 +0530

    clean up wezterm luda

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,30 +1,10 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 return {
-	-- font = wezterm.font('Menlo'),
-	-- font = wezterm.font('Terminess Nerd Font'),
-	-- font = wezterm.font('CozetteVector'),
-	font_size = 15.0,
+	font_size = 14.0,
 	window_decorations='INTEGRATED_BUTTONS|RESIZE',
     window_background_opacity = 0.9,
-	-- window_decorations='TITLE|RESIZE',
     window_close_confirmation='NeverPrompt',
-    --default_cursor_style = 'BlinkingUnderline',
-	keys = {
-		{
-			key = 'f',
-			mods = 'CTRL',
-			action = wezterm.action.ToggleFullScreen,
-		},
-        {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-        {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
-        {
-            key = 't',
-            mods = 'CTRL',
-            action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-        }
-	},
-	-- default_cursor_style = 'SteadyUnderline',
 	cursor_thickness = 2.5,
 	mouse_bindings = {
 	  {
@@ -37,15 +17,12 @@ return {
 	  left = 10,
 	  right = 10,
 	  top = 50,
-	  -- top = 60,
 	  bottom = 10,
     },
-	initial_rows=30,
-	initial_cols=120,
+	initial_rows=26,
+	initial_cols=100,
     colors = {
-	  --cursor_bg = '#cc1111',
 	  cursor_bg = '#cc1111',
-	  --cursor_fg = '#181818',
 	  background = '#010101',
 	  foreground = '#eeeeee'
     },
@@ -55,10 +32,6 @@ return {
 		font = wezterm.font { family = 'Noto Sans', weight = 'Regular' },
 	},
 	keys = {
-		-- { key = 't', mods = 'CMD', action = act.SpawnTab 'CurrentPaneDomain' },
-		--       { key = 'LeftArrow', mods = 'CMD', action = wezterm.action { SendString = "\x1bOH" }, },
-		--       { key = 'RightArrow', mods = 'CMD', action = wezterm.action { SendString = "\x1bOF" }, },
-        -- { mods = "CMD", key = "Backspace", action = act.SendKey({ mods = "CTRL", key = "u" }) },
         { mods = "OPT", key = "LeftArrow", action = act.SendKey({ mods = "ALT", key = "b" }) },
         { mods = "OPT", key = "RightArrow", action = act.SendKey({ mods = "ALT", key = "f" }) },
         { mods = "CMD", key = "LeftArrow", action = act.SendKey({ mods = "CTRL", key = "a" }) },
